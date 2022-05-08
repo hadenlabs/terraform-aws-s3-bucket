d1d9874386a18a41e3359955a6fca0a4910edb45
Luis Mayta <luis@hadenlabs.com>
2022-05-08 09:40:20 -0500

    sample: implement basic (#3)

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,3 +1,23 @@
+module "tags" {
+  source    = "hadenlabs/tags/null"
+  version   = ">=0.2"
+  namespace = var.namespace
+  stage     = var.stage
+  name      = var.name
+  tags      = var.tags
+}
+
 module "main" {
-  source = "../.."
+  source                 = "../.."
+  name                   = module.tags.id_full
+  stage                  = var.stage
+  namespace              = var.namespace
+  acl                    = var.acl
+  versioning_enabled     = var.versioning_enabled
+  force_destroy          = var.force_destroy
+  allowed_bucket_actions = var.allowed_bucket_actions
+  tags                   = module.tags.tags
+  depends_on = [
+    module.tags,
+  ]
 }
---
+++ examples/basic/outputs.tf
@@ -0,0 +1,29 @@
+output "instance" {
+  description = "show instance module"
+  value       = module.main.instance
+}
+
+output "bucket_domain_name" {
+  value       = module.main.bucket_domain_name
+  description = "FQDN of bucket"
+}
+
+output "bucket_regional_domain_name" {
+  value       = module.main.bucket_regional_domain_name
+  description = "The bucket region-specific domain name"
+}
+
+output "bucket_id" {
+  value       = module.main.bucket_id
+  description = "Bucket Name (aka ID)"
+}
+
+output "bucket_arn" {
+  value       = module.main.bucket_arn
+  description = "Bucket ARN"
+}
+
+output "bucket_region" {
+  value       = module.main.bucket_region
+  description = "Bucket region"
+}
